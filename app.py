04c44710e7a1ca524732d8daad296df86d70f3d9
CyberRaksha Initiative <cyberraksha.initiative@gmail.com>
2026-08-09 23:43:27 +0530

    Prepare CamBait for deployment

--- app.py
+++ app.py
@@ -85,9 +85,12 @@ def save_video():
 
 
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
-        host="127.0.0.1",
-        port=5000,
+        host="0.0.0.0",
+        port=port,
         debug=False
     )
